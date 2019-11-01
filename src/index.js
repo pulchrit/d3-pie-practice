133b8d3f8c7ea32df120b06e9e3413298636f16b
Melissa Lafranchise <pulchrit@gmail.com>
2019-11-01 14:33:59 -0700

    app component implemented

--- src/index.js
+++ src/index.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import * as d3 from 'd3';
+import PieClass from "./PieClass";
+import PieHooks from './PieHooks';
+import PieSVG from './PieSVG';
 import './index.css';
-import App from './App';
+
+const App = () => {
+    const generateData = (value, length = 5) => 
+        d3.range(length).map((item, index) => ({
+            date: index,
+            value: value === null || value === undefined ? Math.random() * 100 : value
+        }))
+
+
+    const [data, setData] = useState(generateData());
+
+    const changeData = () => {
+        setData(generateData());
+    }
+
+    return (
+        <div className='app'>
+            <div>
+                <button onClick={changeData}>Transform</button>
+            </div>
+
+            <div>
+                <span className='label'>React Class</span>
+                <PieClass
+                    data={data}
+                    width={200}
+                    height={200}
+                    innerRadius={60}
+                    outerRadius={100}
+                />
+            </div>
+            <div>
+                <span className='label'>Hooks</span>
+                <PieHooks
+                    data={data}
+                    width={200}
+                    width={200}
+                    height={200}
+                    innerRadius={60}
+                    outerRadius={100}
+                />
+            </div>
+            <div>
+                <span className='label'>SVG Elements</span>
+                <PieSVG
+                data={data}
+                width={200}
+                width={200}
+                height={200}
+                innerRadius={60}
+                outerRadius={100} 
+                /> 
+            </div>
+        </div>
+    );
+}
 
 ReactDOM.render(
     <App />, 
